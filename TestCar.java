1af534c8efa7ea28536b5087b25e946d6840fdf2
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2015-10-09 14:15:42 +0700

    TEST CLASS CAR

---
+++ TestCar.java
@@ -0,0 +1,10 @@
+public class TestCar{
+    public static void main(String args[]) {
+        Car toyota = new Car();
+        toyota.setColor("RED");
+        toyota.setWheels(4);
+        toyota.setBrand("VIOS");
+
+        System.out.println("TOYOTA OBJECT = " + toyota);
+    }
+}
